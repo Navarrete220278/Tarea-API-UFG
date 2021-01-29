f2bb4bf4544f10c8e3c81d9f0e04a489222e8120
Ernesto Aguilar <eaguilarjz@gmail.com>
2021-01-29 12:34:59 -0600

    Se agrego ejemplo de propiedades de AWS

---
+++ src/main/java/sv/ufg/ordenaenlinea/OrdenaEnLineaApplication.java
@@ -0,0 +1,13 @@
+package sv.ufg.ordenaenlinea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenaEnLineaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenaEnLineaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sv/ufg/ordenaenlinea/OrdenaEnLineaApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.ufg.ordenaenlinea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdenaEnLineaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
